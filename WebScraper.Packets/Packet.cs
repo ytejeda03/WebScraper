ca9035c338b0181c9d47256ea72d452a11055480
Yunior Tejeda <y.tejeda03@gmail.com>
2019-12-05 01:13:58 -0500

    Packet Structure Implemented, 4 types of packet Welcome, Request, DownloadStatus and Error.

--- WebScraper.Packets/Packet.cs
+++ WebScraper.Packets/Packet.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WebScraper.Packets
 {
+    [Serializable]
     public class Packet
     {
+        public List<string> packetData;
+        public string senderID;
+        public PacketType packetType;
+
+        public Packet(PacketType packetType, string senderID)
+        {
+            this.packetData = new List<string>();
+            this.senderID = senderID;
+            this.packetType = packetType;
+        }
+
+        public Packet(byte[] packetBytes)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream(packetBytes);
+
+            Packet p = (Packet)bf.Deserialize(ms);
+            ms.Close();
+
+            this.packetData = p.packetData;
+            this.senderID = p.senderID;
+            this.packetType = p.packetType;
+
+        }
+
+        public byte[] ToBytes()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+
+            bf.Serialize(ms, this);
+
+            byte[] bytes = ms.ToArray();
+            ms.Close();
+
+            return bytes;
+        }
+
+        public static string GetIp4Address()
+        {
+            IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach (IPAddress i in ips)
+            {
+                if (i.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return i.ToString();
+                }
+            }
+
+            return "127.0.0.1";
+        }
+    }
+
+    public enum PacketType
+    {
+        Welcome,
+        Request,
+        DownloadStatus,
+        Error
     }
 }
